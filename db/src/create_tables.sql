0328f8e853c92dc497df09b8e6763dece3604178
mlamelas <manuel.lamelas@gmail.com>
2016-02-18 10:32:12 +0000

    db

---
+++ db/src/create_tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tgc_maestra
+(
+   pep varchar(50),
+   tenant_id varchar(20),
+   tenant_name varchar(120),
+   policy varchar(50),
+   coste_vcpu numeric(9,9),
+   coste_vram numeric(9,9),
+   coste_stg_perf numeric(9,9),
+   coste_stg_opt numeric(9,9),
+   coste_stg_cap numeric(9,9),
+   coste_stg_extrem numeric(9,9),
+   business_group varchar(50),
+   contratomarco varchar(10)
+)
+;
+
